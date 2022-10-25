5e5426aa1c06fa8f81c33e1af2dd76dad4b9ff0d
Boyan <72163457+boce1@users.noreply.github.com>
2022-10-25 12:39:03 -0700

    Delete school-main directory

--- school-main/vp/lekcija/min_even.py
+++
@@ -1,4 +0,0 @@
-import random
-
-numbers = [random.randint(-100, 100) for x in range(20)]
-
--- school-main/vp/lekcija/min_max.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-numbers = [randint(-100, 100) for x in range(20)]
-
-print(numbers)
-print(min(numbers) + max(numbers))
-
-# lorem ipsum
-# generator na bytove, dumi i spisaci
