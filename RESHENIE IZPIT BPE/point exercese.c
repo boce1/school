772054c7dce5620fb0117072415c1d50f0ba358d
Boyan <72163457+boce1@users.noreply.github.com>
2023-05-30 19:21:45 +0300

    upadte file

--- RESHENIE IZPIT BPE/point exercese.c
+++ RESHENIE IZPIT BPE/point exercese.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define abs_(x) ((x > 0) ? x: -x)
 
 typedef struct point
 {
@@ -10,6 +13,9 @@ typedef struct point
 void printArray(point* arr, const int size);
 void addPoint(point* arr, int* size, int* usedSize, point p);
 void readBinFile();
+int findIndexPoint(point* arr, const int size, point p);
+int area(point* a, point* b);
+float shortestDistance(point* a, point* b);
 
 int main()
 {
@@ -34,6 +40,13 @@ int main()
     printArray(arr, usedSize);
     //printf("%d\n", size);
     readBinFile();
+
+    int in = findIndexPoint(arr, usedSize, c);
+    float side = shortestDistance(&a, &d);
+    //printf("index c point: %d\n", in);
+
+    printf("Area: %d\n", area(&a, &b));
+    printf("Distance a - d: %.2f\n", side);
     free(arr);
     return 0;
 }
@@ -120,4 +133,33 @@ void readBinFile()
         printf("%d %d\n", temp.x, temp.y);
     }
     fclose(fptr);
+}
+
+int findIndexPoint(point* arr, const int size, point p)
+{
+    int index = -1;
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        if(arr[i].x == p.x && arr[i].y == p.y)
+        {
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+
+int area(point* a, point* b)
+{
+    return abs_((a->x - b->x)) * abs_((a->y - b->y));
+}
+
+float shortestDistance(point* a, point* b)
+{
+    float s1, s2;
+    s1 = (float)abs_((a->x - b->x));
+    s2 = (float)abs_((a->y - b->y)); 
+
+    return sqrt(s1*s1 + s2*s2);
 }
