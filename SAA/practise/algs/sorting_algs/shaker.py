01f483fc826b646224db40553e7b1942d9646ae2
Boyan <72163457+boce1@users.noreply.github.com>
2023-10-28 20:02:41 +0300

    Update shaker.py

--- SAA/practise/algs/sorting_algs/shaker.py
+++ SAA/practise/algs/sorting_algs/shaker.py
@@ -2,20 +2,27 @@ from .swap import swap_elements
 
 def shaker_sort(arr):
     left = 0
-    right = len(arr) - 1
-    k = left
+	right = len(arr) - 1
+	last_pos = left
+	k = left
 
-    while left < right:
-        while k < right:
-            if arr[k] > arr[k + 1]:
-                swap_elements(arr, k, k + 1)
-            k += 1
-        right -= 1
+	while left < right:
+		while k < right:
+			if arr[k] > arr[k + 1]:
+				swap(arr, k, k + 1)
+				last_pos = k
+			k += 1
 
-        while k > left:
-            if arr[k] < arr[k - 1]:
-                swap_elements(arr, k, k - 1)
-            k -= 1
-        left += 1
+		right = last_pos
+		k = right
+
+		while k > left:
+			if arr[k] < arr[k - 1]:
+				swap(arr, k, k - 1)
+				last_pos = k
+			k -= 1
+
+		left = last_pos
+		k = left
 
     print("shaker sort: ", end = "")
